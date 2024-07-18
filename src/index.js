c8da4d2262e0ab7760029af195772cd415e17e30
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-07-18 15:52:44 -0300

    main prompt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Olá pessoas')
+import prompt from "prompt";
+import mainPrompt from "./prompts/promp-main.js";
+
+async function main() {
+    prompt.get(mainPrompt, async (err, choice) => {
+        if (choice.select == 1) console.log("Escolheu o QR Code");
+        if (choice.select == 2) console.log("Escolheu o PASSWORD");
+    });
+
+    prompt.start();
+}
+
+await main();
