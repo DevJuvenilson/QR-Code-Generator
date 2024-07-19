5dc08e808fe30da43b01a4436904f773b72c2782
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-07-18 21:25:33 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/promp-main.js";
+import chalk from "chalk";
 
 async function main() {
     prompt.get(mainPrompt, async (err, choice) => {
-        if (choice.select == 1) console.log("Escolheu o QR Code");
-        if (choice.select == 2) console.log("Escolheu o PASSWORD");
+        if (choice.select == 1) console.log(chalk.green("Escolheu o QR Code"));
+        if (choice.select == 2) console.log(chalk.green("Escolheu o PASSWORD"));
     });
 
     prompt.start();
